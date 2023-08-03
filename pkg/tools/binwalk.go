69cef485d9640d0afed36263cdcbe9072ac0331e
neumanlyu <neumannlyu@outlook.com>
2023-08-03 18:34:55 +0800

    兼容macOS

--- pkg/tools/binwalk.go
+++ pkg/tools/binwalk.go
@@ -2,9 +2,12 @@ package tools
 
 import (
     "fmt"
+    "os"
     "os/exec"
     "runtime"
-    "syscall"
+    "strings"
+
+    "github.com/cimercomcn/goiotscanner/pkg/config"
 )
 
 // 利用binwalk工具提取传入的bin文件。
@@ -13,24 +16,29 @@ import (
 // @param 提取后的输出目录
 // @return bool
 func BinwalkMe(bin_path, out_dir string) bool {
+    fmt.Printf("out_dir: %v\n", out_dir)
     var cmd *exec.Cmd = nil
     // 执行binwalk
     // 指定要执行的命令和参数
-    switch os := runtime.GOOS; os {
+    switch osp := runtime.GOOS; osp {
     case "darwin":
+        _cfgPtr = *config.GetConfigInstance()
         cmd = exec.Command("sudo", "binwalk", "-Me", bin_path, "--run-as=root")
+        cmd.Stdin = os.Stdin
+        // fmt.Printf("_cfgPtr.BinExtractedDir: %v\n", _cfgPtr.BinExtractedDir)
+        lastSlashIndex := strings.LastIndex(_cfgPtr.BinExtractedDir, "/")
 
-        // 设置SysProcAttr字段以提升进程权限
-        cmd.SysProcAttr = &syscall.SysProcAttr{
-            Setpgid: true,
+        if lastSlashIndex != -1 {
+            // 使用字符串切片截取最后一个斜杠前面的内容
+            _cfgPtr.BinExtractedDir = _cfgPtr.BinExtractedDir[:lastSlashIndex]
+            // fmt.Println("Path without last segment:", result)
         }
-        // cmd = exec.Command("binwalk", "-Me",
-        //     out_dir, bin_path, "--run-as=root")
+        // fmt.Printf("_cfgPtr.BinExtractedDir: %v\n", _cfgPtr.BinExtractedDir)
     case "linux":
         cmd = exec.Command("binwalk", "-Me", "-C",
             out_dir, bin_path, "--run-as=root")
     }
-    fmt.Printf("cmd.String(): %v\n", cmd.String())
+    // fmt.Printf("cmd.String(): %v\n", cmd.String())
     // 执行命令并等待结果
     output, err := cmd.CombinedOutput()
     if err != nil {
